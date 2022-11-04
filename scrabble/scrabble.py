a71d690c3446ae6b01684dc561cbf08355d7ab71
Mariam-mohamed110 <mariammohamed0313@gmail.com>
2022-11-04 16:47:27 +0000

    bag generated

--- scrabble/scrabble.py
+++ scrabble/scrabble.py
@@ -1,12 +1,25 @@
 # generate bag - maybe use a dictionary?
 import random
+import readline
+# quick search how to open a txt file
 
 # magic number?
-# tileDistribution = dictionary, each letter with its dist.
+tileDistribution = {'E': 12, 'A': 9, 'I': 9, 'O': 8, 'N': 6, 'R': 6, 'T': 6, 'L': 4, 'S': 4, 'U': 4, 'D': 4, 'G': 3, 'B': 2, 'C': 2, 'M': 2, 'P': 2, 'F': 2, 'H': 2, 'V': 2, 'W': 2, 'Y': 2}
+# dictionary, each letter with its dist.
 
-def generateBag():
+def generateBag(letters):
 # variable = empty array
+    bagTiles = []
 # nested loop going through the dictionary for tile distribution
+    for letter in letters:
+        for i in range(letters[letter]):
+            bagTiles.append(letter)
+    
+    return bagTiles
+
+# print(generateBag(tileDistribution))
+# It works yay
+
 
 def generatePoints():
 # assign each letter to its points
@@ -16,6 +29,9 @@ def generatePoints():
 
 def openDictionary():
 #  use with open to open file
+    with open('dictionary.txt') as f:
+        dictionaryTxt = readline(f)
+    return dictionaryTxt
 
 def shuffle():
 # to shuffle the tile in bag
