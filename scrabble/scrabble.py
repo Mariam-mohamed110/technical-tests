a03be16d95f447c9da2e1d44495b2458d74c0636
Mariam-mohamed110 <mariammohamed0313@gmail.com>
2022-11-04 16:35:53 +0000

    rough plan completed, started at 4:20

---
+++ scrabble/scrabble.py
@@ -0,0 +1,29 @@
+# generate bag - maybe use a dictionary?
+import random
+
+# magic number?
+# tileDistribution = dictionary, each letter with its dist.
+
+def generateBag():
+# variable = empty array
+# nested loop going through the dictionary for tile distribution
+
+def generatePoints():
+# assign each letter to its points
+# declare sum variable
+# for loop going over the word
+# if letter is ...: sum is ...
+
+def openDictionary():
+#  use with open to open file
+
+def shuffle():
+# to shuffle the tile in bag
+# use import random function
+
+
+def playScrabble():
+# where the random tiles will be held
+# player gets 7 tiles which get removed from the bag
+# a word is generated which is compared to the dictionary file
+
